50ac3488aec11803a2b71bba5ab0f89ff10a8182
Hossein Mohseni <m.hossein769@gmail.com>
2023-04-07 03:49:20 +0330

    Update proxychecker.py

--- proxychecker.py
+++ proxychecker.py
@@ -3,21 +3,29 @@ import requests
 import os
 
 url = "https://www.aparat.com/"
-proxysorce = "https://raw.githubusercontent.com/Bardiafa/Proxy-Leecher/main/proxies.txt" # the raw text of the leeched proxy from your github repo
+proxysorce = "https://raw.githubusercontent.com/Bardiafa/Proxy-Leecher/main/proxies.txt"
 
 pt = os.path.dirname(__file__)
 good = os.path.join(pt, "good.txt")
+ir = os.path.join(pt, "IR.txt")
 
 def check(proxy):
     print(f"Checking proxy {proxy}...")
     try:
-        response = requests.get(url, proxies={"http": "http://" + str(proxy), "https": "http://" + str(proxy)}, timeout=3)
+        response = requests.get(url, proxies={"http": "http://" + str(proxy), "https": "http://" + str(proxy)}, timeout=10)
         if response.status_code == 200:
             print(f"Proxy {proxy} is working!")
-            with open(good, "r+") as f:
-                contents = f.read()
-                if proxy+'\n' not in contents:
-                    f.write(proxy + "\n")
+            try:
+                cu = requests.get(f"https://ipapi.co/{proxy}/json/", timeout=10).json()
+                if cu["country"] == "IR":
+                    with open(ir, "a") as f:
+                        f.write(proxy + "\n")        
+                else:
+                    with open(good, "a") as f:
+                        f.write(proxy + "\n")
+            except:
+                with open(good, "a") as f:
+                    f.write(proxy + "\n")            
     except (requests.exceptions.ProxyError, requests.exceptions.Timeout, requests.exceptions.ConnectTimeout):
         print(f"Proxy {proxy} is NOT working X")
 
