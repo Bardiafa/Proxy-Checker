2b93bd42f3486ec3ec4d0e7fe725debe3f86396a
Hossein Mohseni <m.hossein769@gmail.com>
2023-04-07 03:50:46 +0330

    Update and rename duplicate-remover.py to remover.py

--- remover.py
+++ remover.py
@@ -3,9 +3,11 @@ import os
 
 pt = os.path.dirname(__file__)
 good = os.path.join(pt, "good.txt")
+ir = os.path.join(pt, "IR.txt")
 
 def empty_good():
     open(good, 'w').close()
+    open(ir, 'w').close()
     print(f"Emptied!")
 
 empty_good()
